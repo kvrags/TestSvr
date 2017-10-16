1f8c655a60a8eaf1ef28a0d92fe942bfd89027fd
kvrags <kvrags@gmail.com>
2017-10-16 14:44:51 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 // server.js set up ======================================================================
 var express = require('express');
+var https = require('https');
+var fs = require ('fs'); 
 var app = express(); 								// create our app w/ express
-var mongoose = require('mongoose'); 					// mongoose for mongodb
+var mongoose = require('mongoose');					// mongoose for mongodb
 var port = process.env.PORT || 8080; 				// set the port
 //var database = require('./config/database'); 			// load the database config
 
@@ -33,13 +35,21 @@ app.use(bodyParser.json()); 									// parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride());
 //app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
-app.use(express.static(__dirname + '../public')); // set the static files location /public/img will be /img for users
 
 /*app.use(function (err, req, res, next) {
     console.error(err.stack); 
     res.status(500); 
     res.render('NeuroGym Server error', { error: err });
 });*/
+
+/* CORS issue
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+*/
+
  
 // REST API routes ======================================================================
 require('./api/routes/profileRoutes.js')(app);
@@ -57,7 +67,17 @@ app.get('*', function (req, res) {
     //res.sendFile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
 });
 
+
 // listen (start app with node server.js) ======================================
-app.listen(port);
+var sslOptions = {key: fs.readFileSync('./config/domain.key'),
+				  cert: fs.readFileSync('./config/domain.crt')};
+
+
+https.createServer(sslOptions, app).listen(8443); 	
+console.log("https server instance listening on port " + 8443);
+	
+	
+/*app.listen(port);
 console.log("App listening on port " + port);
+*/
 exports = module.exports = app;
