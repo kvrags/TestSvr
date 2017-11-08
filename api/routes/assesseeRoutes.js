de72f92af9f1a31d15103f78394f9d8b79c58b30
kvrags <kvrags@gmail.com>
2017-11-09 00:42:54 +0530

    Add files via upload

--- api/routes/assesseeRoutes.js
+++ api/routes/assesseeRoutes.js
@@ -19,15 +19,23 @@ module.exports = function (app) {
     app.route('/assessee')
       .get(assessee.list_all_assessees)
       .post(assessee.create_a_assessee)
+
+	app.route('/assessee/:operation')
 	  .patch(assessee.bulkInsert);
 
 
-    app.route('/assessee/:assesseeId')
-      .get(assessee.read_a_assessee)
+    app.route('/assessee/:email')
+      .get(assessee.find_a_assessee)
       .put(assessee.update_a_assessee)
       .delete(assessee.delete_a_assessee);
+	
+	
+	/*//app.route('/assessee/:name/:email')
+    app.route('/assessee/:email')
+      .get(assessee.find_a_assessee)
+*/
 
-        //console.error(err.stack);
+	  //console.error(err.stack);
         //res.status(500);
         //res.render('NeuroGym Server error in Profiles Controller:', { error: err });
   //  next(err);
